3449934d9f8218ebfe98c2e4aec00b5ce658e79a
pankajku-07 <digitalpankaj52@gmail.com>
2025-09-09 11:10:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+   root: path.resolve(__dirname, 'src'), // <-- Vite root
+  build: {
+    outDir: path.resolve(__dirname, 'dist/public'),
+    emptyOutDir: true,
+  },
   plugins: [
     react(),
     runtimeErrorOverlay(),
